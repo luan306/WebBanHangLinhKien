2cd4fd344160f81d2741a7e329493ce79bf023c5
luan306 <nguyenluan3062001@gmail.com>
2023-02-07 23:37:37 +0700

    v2 giao dien

---
+++ WebBanHang/BanHangLinkKien/BanHangLinkKien/Controllers/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BanHangLinkKien.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebBanHang/BanHangLinkKien/BanHangLinkKien/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using BanHangLinkKien.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BanHangLinkKien.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ShoplinkkienContext _context;
+        public ProductController(ShoplinkkienContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details(int id)
+        {
+            var product=_context.Products.Include(x=>x.Cat).FirstOrDefault(x=>x.ProductId == id);
+            if(product == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+    }
+}
