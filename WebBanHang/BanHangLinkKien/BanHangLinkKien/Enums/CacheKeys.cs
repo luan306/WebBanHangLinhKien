37e46579e3600e633840ecd9c04d6c3a2364cd58
luan306 <nguyenluan3062001@gmail.com>
2023-03-02 23:12:09 +0700

    3/2/2023

---
+++ WebBanHang/BanHangLinkKien/BanHangLinkKien/Enums/CacheKeys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BanHangLinkKien.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BanHangLinkKien.Enums
+{
+    public enum CacheKeys
+    {
+        Categories
+    }
+}
